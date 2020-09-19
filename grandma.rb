94fb1c9120c128a1d85d50f4caffea8f0533bb40
Kevin Dole <dole.kevin@gmail.com>
2020-09-19 04:08:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ def speak_to_grandma(phrase)
   if phrase == "what?".upcase
     return "NO, NOT SINCE 1938!"
   end
-  else phrase == "What did you eat today?"
+  if phrase == "What did you eat today?"
   return "NO, NOT SINCE 1938!"
   end
   
