c48694994d4e2890fa20954941c2ba5b2612a8be
Kevin Dole <dole.kevin@gmail.com>
2020-09-19 03:21:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,6 @@ end
 
 def speak_to_grandma(phrase)
   puts "#{phrase}"
-  if #{"I LOVE YOU GRANDMA!"} == I LOVE YOU GRANDMA!
+  if "#{"I LOVE YOU GRANDMA!"}" == I LOVE YOU GRANDMA!
   puts "I LOVE YOU TOO PUMPKIN!"
 end
