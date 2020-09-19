c5993c145ea97af86373bdaa26b0185aa2d54327
Kevin Dole <dole.kevin@gmail.com>
2020-09-19 01:57:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,7 @@
 
 
 def speak_to_grandma
-if speak_to_grandma == 
-  
-    
   if speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  else
     
-  phrase = speak_to_grandma.upcase
-  puts "HUH?! SPEAK UP, SONNY!"
