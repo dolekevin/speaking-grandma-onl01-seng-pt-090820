875ba817a6d6c904b79c00d6810f2c2926c0e5e9
Kevin Dole <dole.kevin@gmail.com>
2020-09-19 02:01:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,5 @@ def speak_to_grandma
   if speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else if speak_to_grandma.upcase == "NO, NOT SINCE 1938!"
-  else 
+  else speak_to_grandma == 
     
