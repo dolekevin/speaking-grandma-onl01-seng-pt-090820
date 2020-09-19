1729716b2f2ef6d6fc9780dea5fc36945af4b164
Kevin Dole <dole.kevin@gmail.com>
2020-09-19 03:56:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,6 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
   end
-  if phrase == "Hi Nana, how are you?"
-    return 'HUH?! SPEAK UP, SONNY!'
-  end
+  
   
 end
