0c8a81d6289daf96886cc79d424935c4ef4421fa
Kevin Dole <dole.kevin@gmail.com>
2020-09-19 04:00:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
   if phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
   end
-  if phrase == "Hi Nana, how are you?".upcase
+  if phrase == "what?".upcase
     return "NO, NOT SINCE 1938!"
   end
   
